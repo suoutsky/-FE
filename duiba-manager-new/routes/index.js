5011d2d262f5a2f1d34dc8e71e32f6c5de74d87c
nemo <nemo@nemodeMacBook-Pro.local>
2017-02-12 16:58:35 +0800

    add

--- duiba-manager-new/routes/index.js
+++
@@ -1,7 +0,0 @@
-exports.creditgame = function(req, res) {
-  res.redirect('/creditgame.html');
-};
-exports.giftbag = function(req, res) {
-  res.redirect('/giftbag.html');
-};
-
--- duiba-manager-new/src/pages/giftbag/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="app">
-      <div class="main">
-        <router-view></router-view>
-      </div>
-    </div>
-  </div>
-</template>
